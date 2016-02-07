0dc3a16c12ecaa36d6156f4df49ad01ac5659fc9
parkerclayton <parkerclayton@comcast.net>
2016-02-06 23:29:44 -0800

    finished SW carpentry loop practice

--- SW_carpentry/02_Repeating_Actions_With_Loops.py
+++ SW_carpentry/02_Repeating_Actions_With_Loops.py
@@ -1,7 +1,25 @@
 #! /usr/bin/python
 
 
+#counts number of vowels in the string and prints them
 length = 0
 for vowel in 'aeiou':
 	length = length +1
-print('There are', length, 'vowels')
+print('There are', length, 'vowels')             
+
+print(len('aeiou'))
+
+
+#computes an exponent
+for x in range(4):
+	x=5**(x)
+print(x)
+
+
+#takes a string and reverses it
+N="Parker"
+
+for x in range(len(N)):
+	y=len(N)-x
+	str=N[y-1]
+	print(str)
