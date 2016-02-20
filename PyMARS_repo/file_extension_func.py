af2f3d6e29b914a7840cd70b1f5b4df75c8f056f
parkerclayton <parkerclayton@comcast.net>
2016-02-19 19:17:35 -0800

    working on an overhead function to evaluate filetype, and then call appropriate read-in function

--- PyMARS_repo/file_extension_func.py
+++ PyMARS_repo/file_extension_func.py
@@ -1,8 +1,14 @@
 # this function reads in the data file, and determines what type of file it is
 
+
+import cantera as ct
+import os
+
 def readin(data_file):
 	if data_file.endswith(".xml"):
 		print("This is an Cantera xml file")
+		from xml_readin_func import xmlreadin
+		xmlreadin(data_file)
 	elif data_file.endswith(".inp"):
 		print("This is a Chemkin inp file")
 	elif data_file.endswith(".cti"):
@@ -11,4 +17,5 @@ def readin(data_file):
 		print("File type not supported")
 		
 
-	
+
+readin("gri30.xml")
--- PyMARS_repo/xml_readin_func.py
+++ PyMARS_repo/xml_readin_func.py
@@ -1,24 +1,32 @@
-#this function reads in species from a .xml file and prints them out
+#series of functions to work with Cantera xml file
 
 import cantera as ct
+import os
+
+
+
+#function to read in an xml file and print out species names
 def xmlreadin(data_file):
 	spec= ct.Species.listFromFile(data_file)
 	print(spec)
 
 
 
-	
-#dummy list of species
-spec=[ 'H4' , 'CO2' , 'C']
-exc=[ 'Species H2', 'Species H', 'Species O']
-	
+#commented out for now. everything above works	
+"""#dummy list of species
+spec=[ 'H4' , 'CO2' , 'C']	
 #list of species to exclude	
-	
+exc=[ 'H4', 'H', 'O']
+
+
 #compares two lists of species, and removes the species in the second list from the first
 def comparelist( spec, spec_exclude):
 	for val in spec_exclude:
 		if val in spec:
 			spec.remove(val)
-			print(spec) 
+	print(spec) 
 
+#import species from xml (use this to import 
+species= ct.Species('gri30:all') 
 
+print(species)"""
