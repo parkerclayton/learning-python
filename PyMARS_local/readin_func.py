f8934aab50094286c7dab4d7cf4b056b95a7902f
parkerclayton <parkerclayton@comcast.net>
2016-03-08 11:21:26 -0800

    local repo, cleaned up extra code in readin func

--- PyMARS_local/readin_func.py
+++ PyMARS_local/readin_func.py
@@ -59,48 +59,4 @@ xmlreadin("gri30.xml", SPexc)
 			
 			
 			
-			
-			
 	
-
-#ignore everything below -----------------------------------------------------
-
-
-#calling the function
-#list to exclude
-"""SPexc=['H2', 'H'];
-
-xmlreadin("gri30.xml", SPexc)"""
-
-
-	#compare-list to be used later
-"""for val in exclusion_list:
-		if val in Species:
-			Species.remove(val)
-	print(Species)"""
-
-
-
-#Species= ct.Species.listFromFile(data_file)
-	#print(Species)  
-
-
-
-#old reference...commented out for now.
-"""#dummy list of species
-spec=[ 'H4' , 'CO2' , 'C']	
-#list of species to exclude	
-exc=[ 'H4', 'H', 'O']
-
-
-#compares two lists of species, and removes the species in the second list from the first
-def comparelist( spec, spec_exclude):
-	for val in spec_exclude:
-		if val in spec:
-			spec.remove(val)
-	print(spec) 
-
-#import species from xml (use this to import 
-species= ct.Species('gri30:all') 
-
-print(species)"""
