818fd6a3061c9ca76a7ac5189e6dcf735cdacdac
parkerclayton <parkerclayton@comcast.net>
2016-05-31 01:20:06 -0700

    used kinematic similarities to extrapolate model pump curve to prototype pump curve

--- ME 332 model graph.py
+++ ME 332 model graph.py
@@ -20,29 +20,46 @@ import matplotlib.pyplot as plt
 1.76, 3.93
 1.96, 3.08
 """
-
-Q=np.array([0.17, 0.35, 0.61, 0.88, 1.11, 1.31, 1.51, 1.76, 1.96 ])
-
-dP=np.array([6.62, 6.65, 6.59, 6.30, 5.88, 5.36, 4.85, 3.93, 3.08])
-
+pi=np.pi
+"""-------------------------------------------------------------------------
+                        Model Pump
+-------------------------------------------------------------------------"""
+Q1_raw=np.array([0.17, 0.35, 0.61, 0.88, 1.11, 1.31, 1.51, 1.76, 1.96 ])
+dP1_raw=np.array([6.62, 6.65, 6.59, 6.30, 5.88, 5.36, 4.85, 3.93, 3.08])
+w1= 40*pi          #radians/second
+d1= 8.0/12.0             #feet
 # fit a curve
-z=np.polyfit(Q, dP, 2)
-y=np.poly1d(z)
+z1=np.polyfit(Q1_raw, dP1_raw, 2)
+y1=np.poly1d(z1)
 #get curve points
-zp=np.linspace(0, 3, 100)
-yp=y(zp)
+Q1=np.linspace(0, 3, 100)
+dP1=y1(Q1)
+
+"""-------------------------------------------------------------------------
+                        Prototype Pump
+-------------------------------------------------------------------------"""
+w2= 60*pi          #radians/second
+d2= 1             #feet
+
+Q2= Q1*(w2/w1)*((d2/d1)**3)
+dP2= dP1*((w2/w1)**2)*((d2/d1)**2)
+
+
+
 
 
 "plot visuals"
-plt.axis([0, 3.0, 0, 8])
+plt.axis([0, 14.0, 0, 35])
 plt.title('Centrifugal Pump - Model')
 plt.xlabel('Q (ft^3/s)')
 plt.ylabel('dP (psi)')
 plt.grid(True)
 
 "plot the data"
-plt.plot(Q, dP, 'ko ')
-plt.plot(zp, yp, lw=2)
+plt.plot(Q1_raw, dP1_raw, 'ko ')
+plt.plot(Q1, dP1, lw=2, label='model pump')
+plt.plot(Q2, dP2, 'k', lw=2, label='prototype pump')
+plt.legend()
 plt.show()
 
 #given flow rate, want to know dP
